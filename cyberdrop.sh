78a40b9f43281fd7bd32ed89a11d7a11eec31168
s4nket <sanketrajbhar63@gmail.com>
2022-01-26 02:28:15 +0530

    refactor

--- cyberdrop.sh
+++ cyberdrop.sh
@@ -43,31 +43,25 @@ while getopts "hvc:" OPT; do
 done
 shift $((OPTIND - 1))
 
-[[ -z "$@" ]] && { echo -n "Enter link: "; read sauce; } || sauce="$@"
-[[ -z $sauce ]] && { echo "[-] No links given."; exit 1; }
+[[ -z "$@" ]] && { echo -n "Enter link: "; read source; } || source="$@"
+[[ -z $source ]] && { echo "[-] No links given."; exit 1; }
 
-for i in ${sauce[@]}; do
+for i in ${source[@]}; do
 
 	[[ ! $(curl -sLI $i | head -1) =~ 200 ]] && { echo "[-] Wrong link :: $i"; continue; }
 	html=$(curl -s $i)
-	title=$(echo $html | pup.exe 'h1#title attr{title}')
+	title=$(pup.exe 'h1#title attr{title}' <<< $html)
 	[[ -d "$title" ]] && [[ ! -z $(ls -A "$title") ]] && { echo "[-] Already Downloaded :: $title [$i]"; continue; }
-	tmp=$(echo $html | pup.exe 'p.title text{}')
-	files=$(echo $tmp | awk '{print $1}')
-	size=$(echo $tmp | awk '{print $2" "$3}')
-	links=$(echo $html |\
-		pup.exe 'a.image attr{href}' |\
-		sed 's/[[:space:]]/\%20/g')
+	files=$(pup.exe 'p#totalFilesAmount text{}' <<< $html)
+	size=$(pup.exe 'nav.level > div:nth-child(2) > div > p.title text{}' <<< $html)
+	links=$(pup.exe 'a.image attr{href}' <<< $html | sed 's/[[:space:]]/\%20/g')
 
 	printf "[*] Downloading :: [Album: $title :: $files :: $size]" && mkdir -p "$title"
-	echo $links |\
-		sed 's/[[:space:]]/\n/g' |\
-		xargs -P $MAX -I{} aria2c -q -x 5 -d "$title" {}
+	sed 's/[[:space:]]/\n/g' <<< $links | xargs -P $MAX -I{} aria2c -q -x 5 -d "$title" {}
 #	Use this when new line
 #	printf "\033[A\33[2K%s\n" "[+] Downloaded [Album: $title :: $files :: $size]"
 	printf "\33[2K\r%s\n" "[+] Downloaded [Album: $title :: $files :: $size]"
 #	Use when what fixed length output
 #	printf "%-100s\r" "[+] Downloaded [Album: $title :: $files :: $size]"
 	unset html title files size links
-
 done
